f46b147b2f74afe668d9ee2a46ede3da8c89d071
andre goncalves <andre.g.araujo@live.com>
2018-08-03 22:18:49 -0300

    Consumindo serviços para detalhes do filme

---
+++ src/main/java/br/com/caelum/ingresso/rest/ImdbClient.java
@@ -0,0 +1,31 @@
+package br.com.caelum.ingresso.rest;
+
+import java.util.Optional;
+
+import org.jboss.logging.Logger;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import br.com.caelum.ingresso.model.DetalhesDoFilme;
+import br.com.caelum.ingresso.model.Filme;
+
+@Component
+public class ImdbClient {
+
+	private Logger logger = Logger.getLogger(ImdbClient.class);
+	
+	public Optional<DetalhesDoFilme> request (Filme filme){
+		
+		RestTemplate client = new RestTemplate();
+		
+		String titulo = filme.getNome().replace(" ", "+");
+		
+		String url = String.format("https://imdb-fj22.herokuapp.com/imdb?title=%s", titulo);
+		
+			DetalhesDoFilme detalhesDoFilme = client.getForObject(url, DetalhesDoFilme.class);
+			return Optional.of(detalhesDoFilme);
+		
+		
+	}
+	
+}
